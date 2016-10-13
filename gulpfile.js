ef933a24491001c288d433ebeb97c87f3e494afd
Sarah Waldie <sareliz126@gmail.com>
2016-10-13 14:32:45 -0400

    add image folder

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+
+var sassOptions = {
+	errLogToConsole: true,
+	outputStyle: 'expanded'
+};
+
+var sassSources = './scss/**/*.scss';
+var sassOutput = './app/css';  
+var htmlSource = 'app/**/*.html';
+var jsSource = 'app/js/*.js';  
+
+gulp.task('sass', function(){
+	return gulp.src(sassSources)
+	.pipe(sourcemaps.init())
+	.pipe(sass(sassOptions).on('error', sass.logError))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest(sassOutput))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('serve', ['sass'], function(){
+
+	browserSync.init({
+		server: './app'
+	})
+
+	gulp.watch(sassSources, ['sass']);
+	gulp.watch(htmlSource, browserSync.reload);
+	gulp.watch(jsSource, browserSync.reload);
+});
+
+
+gulp.task('default', ['serve']);
